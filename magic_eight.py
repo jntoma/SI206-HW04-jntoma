da40b4354b8d7996b8039266f7901014e17d83df
jntoma <jntoma@umich.edu>
2018-01-31 16:52:08 -0500

    wrote code to check question

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,9 @@
+def CheckQuestion():
+    if question[-1] == "?":
+        print(picked_answer)
+    else:
+        print("I'm sorry, I can only answer questions!")
+
 def Question():
     question = input("What is your question?" )
     return question
