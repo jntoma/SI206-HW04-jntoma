f3b47de85d7a0c222073db979cac7674b276578b
Catherine Xu <catherinexu@0587376227.wireless.umich.net>
2018-01-31 17:15:22 -0500

    random pick function

--- magic_eight.py
+++ magic_eight.py
@@ -3,12 +3,14 @@ def Question():
     question = input("What is your question?" )
     return question
 
-answers = ["It is certain", "As I see it, yes", "Reply hazy try again",
-            "Don't count on it", "It is decidedly so", "Most likely",
-            "Ask again later", "My reply is no", "Without a doubt",
-            "Outlook good", "Better not tell you now", "My sources say no",
-            "Yes definitely", "Yes","Cannot predict now", "Outlook not so good",
-            "You may rely on it", "Signs point to yes", "Concentrate and ask again",
-            "Very doubtful"]
+def random_answer():
+    answers = ["It is certain", "As I see it, yes", "Reply hazy try again",
+                "Don't count on it", "It is decidedly so", "Most likely",
+                "Ask again later", "My reply is no", "Without a doubt",
+                "Outlook good", "Better not tell you now", "My sources say no",
+                "Yes definitely", "Yes","Cannot predict now", "Outlook not so good",
+                "You may rely on it", "Signs point to yes", "Concentrate and ask again",
+                "Very doubtful"]
 
-picked_answer = random.choice(answers)
+    picked_answer = random.choice(answers)
+    return picked_answer
