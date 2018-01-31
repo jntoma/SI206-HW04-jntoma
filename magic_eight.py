435956cb7f56500c10ef5d17d26165c05990043d
Catherine Xu <catherinexu@0587376227.wireless.umich.net>
2018-01-31 16:52:30 -0500

    randomly pick one answer

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,14 @@
+import random
 def Question():
     question = input("What is your question?" )
     return question
+
+answers = ["It is certain", "As I see it, yes", "Reply hazy try again",
+            "Don't count on it", "It is decidedly so", "Most likely",
+            "Ask again later", "My reply is no", "Without a doubt",
+            "Outlook good", "Better not tell you now", "My sources say no",
+            "Yes definitely", "Yes","Cannot predict now", "Outlook not so good",
+            "You may rely on it", "Signs point to yes", "Concentrate and ask again",
+            "Very doubtful"]
+
+picked_answer = random.choice(answers)
