f7a846f9e4570abce646ca70d8bc8a7f181b84ef
jntoma <jntoma@umich.edu>
2018-01-31 17:15:28 -0500

    stop running when quit

--- magic_eight.py
+++ magic_eight.py
@@ -1,9 +1,9 @@
-def CheckQuestion():
-    if question[-1] == "?":
-        print(picked_answer)
-    else:
-        print("I'm sorry, I can only answer questions!")
-
 def Question():
-    question = input("What is your question?" )
+    question = input("What is your question? ")
     return question
+
+while Question() != "quit":
+    if question[-1] != "?":
+        print("I'm sorry, I can only answer questions!")
+    else:
+        print(picked_answer)
